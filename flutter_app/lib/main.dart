b1e769d1c1ada20ed10c832d7e15b95b42a3ce84
MWulff <web@markuswulff.de>
2026-04-15 11:13:57 +0200

    feat(theme): replace AppTheme.theme with buildMaterialTheme(EarnWiseTheme)

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'theme/app_theme.dart';
+import 'theme/theme_catalog.dart';
 import 'state/app_state.dart';
 import 'screens/splash_screen.dart';
 
@@ -25,7 +26,7 @@ class EarnWiseApp extends StatelessWidget {
       create: (_) => AppState(),
       child: MaterialApp(
         title: 'EarnWise',
-        theme: AppTheme.theme,
+        theme: AppTheme.buildMaterialTheme(kCreamTheme),
         debugShowCheckedModeBanner: false,
         home: const SplashScreen(),
       ),
